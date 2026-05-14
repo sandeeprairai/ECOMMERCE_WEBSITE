bda7009783f588f1ad2a275a88023e4d5f4e5b0c
sandeeprairai <sandeeprairai339@gmail.com>
2026-05-14 22:43:30 +0530

    Added Model and set Tailwind css

---
+++ backend/store/views.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product,Category
+from .serializers import productSerilzer,CategorySerializer
+
+# Create your views here.
+@api_view(['GET'])
+def get_products(request):
+    products=Product.objects.all()
+    serializer=productSerilzer(products,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_categories(request):
+    categories=Category.objects.all()
+    serializer=CategorySerializer(categories,many=True)
+    return Response(serializer.data)
+
+
